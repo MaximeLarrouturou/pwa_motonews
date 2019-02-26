2fa52a4e6803c418ad7bdecd7c6f05ac6d5669b5
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-02-26 17:12:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-console.log('hello depuis main');
+console.log('main ok');
 const motosDiv = document.querySelector('#motos');
+
 
 //
 //La database est retirer, pour que le fichier db.json joue son rôle avec un run de jsonserver
@@ -21,15 +22,20 @@ function loadTechnologies(motos) {
         .then(response => { //envoi de la promesse
             response.json() // réponse à la promesse du JSON
                 .then(motos => { //si la promesse est résolu il envoi le JSON
-                    const allMotos = motos.map(t => `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__title"><h2 class="mdl-card__title-text">${t.name}</h2></div><div class="mdl-card__supporting-text">${t.description}</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="${t.url}">site de ${t.name}</a></div><div class="mdl-card__menu"><button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">share</i></button></div></div></div>`)
+                const allMotos = motos.map(t => `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__title"><h2 class="mdl-card__title-text">${t.name}</h2></div><div class="mdl-card__supporting-text">${t.description}</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="${t.url}">site de ${t.name}</a></div><div class="mdl-card__menu"><button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"><i class="material-icons">share</i></button></div></div></div>`)
                         .join('');
 
                     motosDiv.innerHTML = allMotos; 
+                    console.log('Liste de  motos ', motos);
                 });
             })
             // Si motos est rejetée
             .catch(console.error);
+
+            
 }
+
+
 
 loadTechnologies(motos);
 
