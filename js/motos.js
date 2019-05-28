ac4435d987e5596df2ab7ee811490fb7234f36ca
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-05-28 11:29:16 +0200

    Change response JSON

--- js/motos.js
+++ js/motos.js
@@ -9,10 +9,34 @@ function loadTechnologies(motos) {
         .then(response => { //envoi de la promesse
             response.json() // réponse à la promesse du JSON
                 .then(motos => { //si la promesse est résolu il envoi le JSON
-                const allMotos = motos.map(t => `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__title"><img src="${t.blanket}"/><h2 class="mdl-card__title-text">${t.name}</h2></div><div class="mdl-card__supporting-text">${t.description}</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" href="${t.url}" target="_blank">site de ${t.name}</a></div><div class="mdl-card__menu"></div></div></div>`)
+                const allMotos = motos.map(t => `
+                <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+                <div class="demo-card-square mdl-card mdl-shadow--2dp">
+                    <div class="mdl-card__title mdl-card--expand">
+                        <h2 class="mdl-card__title-text">${t.name}</h2>
+                    </div>
+                    <div class="mdl-card__supporting-text">
+                        ${t.description}  
+                    </div>
+                    <div class="mdl-card__actions mdl-card--border">
+                        <a href="${t.url}" target="_blank" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">site de ${t.name}</a>
+                        <div class="mdl-layout-spacer"></div>
+                            <i class="material-icons">public</i>
+                    </div>
+                </div>
+            </div>
+            `)
                         .join('');
 
-                    motosDiv.innerHTML = allMotos + `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card addArticle mdl-shadow--2dp"><a href="add_sbk.html"><button id="tt3" class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"><i class="material-icons">add</i></button></a></div></div>`;
+                    motosDiv.innerHTML = allMotos + `
+                    <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+                        <div class="demo-card-square mdl-card mdl-shadow--2dp addArticle">
+                                <a href="./pages/add_moto.html">
+                                    <button id="tt3" class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"><i class="material-icons">add</i></button>
+                                </a>
+                            </div>
+                        </div>
+                    `;
                     console.log('Liste de  motos ', motos);
                 });
         })
--- js/sbk.js
+++ js/sbk.js
@@ -10,10 +10,37 @@ function loadTechnologies(sbk) {
         .then(response => { //envoi de la promesse
             response.json() // réponse à la promesse du JSON
                 .then(sbk => { //si la promesse est résolu il envoi le JSON
-                    const allSbk = sbk.map(t => `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card mdl-shadow--2dp"><div class="mdl-card__title"><img src="${t.blanket}"/><h2 class="mdl-card__title-text">${t.name}</h2></div><div class="mdl-card__supporting-text">${t.description}</div><div class="mdl-card__actions mdl-card--border"><a class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" href="${t.url}" target="_blank">site de ${t.name}</a></div><div class="mdl-card__menu"><a href="teams.html"><button class="mdl-button mdl-js-button mdl-button--accent">voir les teams</button></a></div></div></div>`)
+                    const allSbk = sbk.map(t => `
+                <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+                    <div class="demo-card-square mdl-card mdl-shadow--2dp">
+                        <div class="mdl-card__title mdl-card--expand">
+                            <h2 class="mdl-card__title-text">${t.name}</h2>
+                        </div>
+                        <div class="mdl-card__supporting-text">
+                            ${t.description}  
+                        </div>
+                        <div class="mdl-card__actions mdl-card--border">
+                            <a href="${t.url}" target="_blank" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">site de ${t.name}</a>
+                            <div class="mdl-layout-spacer"></div>
+                                <i class="material-icons">public</i>
+                        </div>
+                        <div class="mdl-card__menu">
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect white">voir les teams</button>
+                        </div>
+                    </div>
+                </div>
+                `)
                         .join('');
 
-                    sbkDiv.innerHTML = allSbk + `<div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet"><div class="demo-card-wide mdl-card addArticle mdl-shadow--2dp"><a href="add_sbk.html"><button id="tt3" class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"><i class="material-icons">add</i></button></a></div></div>`;
+                    sbkDiv.innerHTML = allSbk + `
+                        <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+                        <div class="demo-card-square mdl-card mdl-shadow--2dp addArticle">
+                                <a href="add_sbk.html">
+                                    <button id="tt3" class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"><i class="material-icons">add</i></button>
+                                </a>
+                            </div>
+                        </div>
+                    `;
 
                     console.log('Liste de  motos ', sbk);
                 });
